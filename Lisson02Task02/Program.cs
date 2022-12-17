6725c04a88b62170da1863bfe29681c445b0ed9c
OlgaGrig74 <OlgaGrig74>
2022-12-17 09:56:29 +0300

    задача 2 готова

--- Lisson02Task02/Program.cs
+++ Lisson02Task02/Program.cs
@@ -5,3 +5,16 @@
 // 3. Составить новое число
 // 4. Показать его на экран
 
+Console.WriteLine("Start");
+
+int value = new Random().Next(100, 1000);  // [100;999]
+
+int a = value / 100;
+int b = (value / 10) % 10;
+int c = value % 10;
+
+int result = a * 10 + c;
+
+Console.WriteLine(value);
+Console.WriteLine(result);
+Console.WriteLine("end");
